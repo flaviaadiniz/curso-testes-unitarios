d2c2b0383b734e22a4636786965bf401d10502b8
flaviaadiniz <flaviaadiniz@gmail.com>
2024-04-13 21:35:47 -0300

    Initial commit

---
+++ src/main/java/com/flavia/cursotestesunitarios/CursoTestesUnitariosApplication.java
@@ -0,0 +1,13 @@
+package com.flavia.cursotestesunitarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoTestesUnitariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoTestesUnitariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flavia/cursotestesunitarios/CursoTestesUnitariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flavia.cursotestesunitarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoTestesUnitariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
