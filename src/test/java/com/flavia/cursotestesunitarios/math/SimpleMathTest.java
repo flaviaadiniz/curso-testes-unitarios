5c0c1a496d5967ef2e500b0164a92c67ae7b7cbd
flaviaadiniz <flaviaadiniz@gmail.com>
2024-04-16 21:42:42 -0300

    Add new tests on SimpleMathTest

--- src/test/java/com/flavia/cursotestesunitarios/math/SimpleMathTest.java
+++ src/test/java/com/flavia/cursotestesunitarios/math/SimpleMathTest.java
@@ -2,21 +2,78 @@ package com.flavia.cursotestesunitarios.math;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SimpleMathTest {
 
+    private static final SimpleMath MATH = new SimpleMath();
+
     @Test
     void testSum() {
-
-        SimpleMath math = new SimpleMath();
-        Double result = math.sum(2.5, 3.5);
+        Double actual = MATH.sum(2.5, 3.5);
         double expected = 6.0;
 
-        assertEquals(expected, result, "testSum() did not produce the expected result.");
+        assertEquals(expected, actual, "testSum() did not produce the expected result.");
         // o parâmetro message só é impresso quando o teste falha
 
-        System.out.println("Expected: " + expected + " | Result: " + result);
+        assertNotEquals(6.2, actual);
+
+        System.out.println("Expected: " + expected + " | Actual: " + actual);
+    }
+
+    @Test
+    void testSubtraction() {
+        Double actual = MATH.subtraction(10.0, 5.0);
+        double expected = 5.0;
+
+        assertEquals(expected, actual, "testSubtraction() did not produce the expected result.");
+        assertNotNull(actual);
+
+        System.out.println("Expected: " + expected + " | Actual: " + actual);
+    }
+
+    @Test
+    void testMultiplication() {
+        Double actual = MATH.multiplication(10.0, 5.0);
+        double expected = 50.0;
+
+        assertEquals(expected, actual, "testMultiplication() did not produce the expected result.");
+        assertNotNull(actual);
+
+        System.out.println("Expected: " + expected + " | Actual: " + actual);
+    }
+
+    @Test
+    void testDivision() {
+        Double actual = MATH.division(10.0, 5.0);
+        double expected = 2.0;
+
+        assertEquals(expected, actual, "testDivision() did not produce the expected result.");
+        assertNotNull(actual);
+
+        System.out.println("Expected: " + expected + " | Actual: " + actual);
+    }
+
+    @Test
+    void testMean() {
+        Double actual = MATH.mean(10.0, 5.0);
+        double expected = 7.5;
+
+        assertEquals(expected, actual, "testMean() did not produce the expected result.");
+        assertNotNull(actual);
+
+        System.out.println("Expected: " + expected + " | Actual: " + actual);
+    }
+
+    @Test
+    void testSquareRoot() {
+        Double actual = MATH.squareRoot(4.0);
+        double expected = 2.0;
+
+        assertEquals(expected, actual, "testSquareRoot() did not produce the expected result.");
+        assertNotNull(actual);
+
+        System.out.println("Expected: " + expected + " | Actual: " + actual);
     }
 
 }
