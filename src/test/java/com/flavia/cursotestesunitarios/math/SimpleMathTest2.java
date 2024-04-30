de470d73b3923750d541f05f838210fcff6a332c
flaviaadiniz <flaviaadiniz@gmail.com>
2024-04-29 21:55:57 -0300

    Add new SimpleMathTest class to test Parameterized Tests with Method Source

---
+++ src/test/java/com/flavia/cursotestesunitarios/math/SimpleMathTest2.java
@@ -0,0 +1,49 @@
+package com.flavia.cursotestesunitarios.math;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SimpleMathTest2 {
+
+    SimpleMath math;
+
+    @BeforeEach
+    void beforeEach() {
+        math = new SimpleMath();
+        System.out.println("Running @BeforeEach method");
+    }
+
+
+    @DisplayName("Test - Division")
+    @ParameterizedTest
+    @MethodSource("testDivisionInputParameter")
+    void testDivision_WhenTenIsDividedByFive_ShouldReturnTwo(double firstNumber, double secondNumber, double expected) {
+
+        // when
+        Double actual = math.division(firstNumber, secondNumber);
+
+        // then
+        assertEquals(expected, actual, () -> firstNumber + "/" + secondNumber + "did not produce " + expected +"!");
+        assertNotNull(actual);
+
+        System.out.println("Expected: " + expected + " | Actual: " + actual);
+    }
+
+
+    public static Stream<Arguments> testDivisionInputParameter() {
+        return Stream.of(
+          Arguments.of(10.0, 2.0, 5.0),
+          Arguments.of(8.0, 2.0, 4.0),
+          Arguments.of(7.5, 3.0, 2.5),
+          Arguments.of(9.0, 3.0, 3.0)
+        );
+    }
+
+
+}
