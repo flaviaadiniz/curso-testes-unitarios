05f7293309946c4bfd0e83f29863fcacb9b97968
flaviaadiniz <flaviaadiniz@gmail.com>
2024-05-02 16:34:31 -0300

    Add testValueSource

--- src/test/java/com/flavia/cursotestesunitarios/math/SimpleMathTest2.java
+++ src/test/java/com/flavia/cursotestesunitarios/math/SimpleMathTest2.java
@@ -2,10 +2,7 @@ package com.flavia.cursotestesunitarios.math;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.*;
 
 import java.util.stream.Stream;
 
@@ -18,6 +15,13 @@ public class SimpleMathTest2 {
     @BeforeEach
     void beforeEach() {
         math = new SimpleMath();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Pelé", "Senna", "Keith Moon"})
+    void testValueSource(String firstName) {
+        System.out.println(firstName);
+        assertNotNull(firstName);
     }
 
 
