6586440ad40ba6ad1de92aa59058bd2396df1a81
flaviaadiniz <flaviaadiniz@gmail.com>
2024-05-09 22:13:12 -0300

    Add MethodOrderByIndexTest to practice annotation that runs test ordered by a given index

---
+++ src/test/java/com/flavia/cursotestesunitarios/MethodOrderByIndexTest.java
@@ -0,0 +1,35 @@
+package com.flavia.cursotestesunitarios;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class MethodOrderByIndexTest {
+
+    @Test
+    @Order(0)
+    void testC() {
+        System.out.println("Running Test C");
+    }
+
+    @Test
+    @Order(2)
+    void testB() {
+        System.out.println("Running Test B");
+    }
+
+    @Test
+    @Order(1)
+    void testA() {
+        System.out.println("Running Test A");
+    }
+
+    @Test
+    @Order(3)
+    void testD() {
+        System.out.println("Running Test D");
+    }
+
+}
