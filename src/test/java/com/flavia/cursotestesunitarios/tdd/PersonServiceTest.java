8e964ea6c93b988df84132b94bfe08d57b56087e
flaviaadiniz <flaviaadiniz@gmail.com>
2024-05-12 20:09:34 -0300

    Add new packages to practice TDD

---
+++ src/test/java/com/flavia/cursotestesunitarios/tdd/PersonServiceTest.java
@@ -0,0 +1,20 @@
+package com.flavia.cursotestesunitarios.tdd;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PersonServiceTest {
+
+    @DisplayName("When Create a Person with Sucess should Return a Person Object")
+    @Test
+    void testCreatePerson_WhenSuccess_shouldReturnAPersonObject(){
+        //given
+
+
+        //when
+
+
+        //then
+    }
+
+}
