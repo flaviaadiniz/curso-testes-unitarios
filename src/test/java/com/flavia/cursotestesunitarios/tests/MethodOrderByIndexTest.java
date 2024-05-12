8a916a74579b96c79eba3b00a66f89d8a2d7cb77
flaviaadiniz <flaviaadiniz@gmail.com>
2024-05-11 21:44:53 -0300

    Add @TestInstance to use per class lifecycle

--- src/test/java/com/flavia/cursotestesunitarios/tests/MethodOrderByIndexTest.java
+++ src/test/java/com/flavia/cursotestesunitarios/tests/MethodOrderByIndexTest.java
@@ -1,36 +1,47 @@
 package com.flavia.cursotestesunitarios.tests;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
-@Order(2)
+//@Order(2)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+// vai criar uma instância única para a classe e vai executar todos os métodos, podendo usar no método 2 algo que foi
+// usado no método 1, por exemplo - muito utilizado em testes de integração
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MethodOrderByIndexTest {
 
-    @Test
-    @Order(0)
-    void testC() {
-        System.out.println("Running Test C");
-    }
+    StringBuilder actualValue = new StringBuilder();
 
-    @Test
-    @Order(2)
-    void testB() {
-        System.out.println("Running Test B");
+    @AfterEach
+    void afterEach() {
+        System.out.println("The actual value is: " + actualValue);
     }
 
     @Test
     @Order(1)
-    void testA() {
-        System.out.println("Running Test A");
+    void testC() {
+        System.out.println("Running Test C");
+        actualValue.append("1");
     }
 
     @Test
     @Order(3)
+    void testB() {
+        System.out.println("Running Test B");
+        actualValue.append("3");
+    }
+
+    @Test
+    @Order(2)
+    void testA() {
+        System.out.println("Running Test A");
+        actualValue.append("2");
+    }
+
+    @Test
+    @Order(4)
     void testD() {
         System.out.println("Running Test D");
+        actualValue.append("4");
     }
 
 }
